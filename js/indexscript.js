20484727fdab0b600151bcd992aefaef50ff74fa
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-08-01 11:30:40 +0800

    fix index page

--- js/indexscript.js
+++ js/indexscript.js
@@ -11,8 +11,8 @@ const initialX = -450;
 const initialY = 0;
 
 // Animation parameters
-const startScroll = 50;
-const endScroll = 250;
+const startScroll = 100;
+const endScroll = 400;
 
 function updateAnimation() {
     const scrollY = window.pageYOffset;
@@ -59,8 +59,8 @@ const initialXskill = -550;
 const initialYskill = 0;
 
 // Animation parameters
-const startScrollskill = 400;
-const endScrollskill = 700;
+const startScrollskill = 500;
+const endScrollskill = 1000;
 
 function updateAnimationskill() {
     const scrollYskill = window.pageYOffset;
